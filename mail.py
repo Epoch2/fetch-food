2f13d31dc157e1a70280349456f870bd1751365d
Johan Vester <jv@jvester.se>
2013-02-16 02:50:30 +0100

    Fixed mailing

--- mail.py
+++ mail.py
@@ -3,10 +3,4 @@ import config
 import passwd
 
 def sendmail(subject, content):
-     os.system(("sendemail -q -f " + config.EMAIL_FROM +
-                " -t " + config.EMAIL_TO +
-                " -s " + config.EMAIL_SERVER +
-                " -xu " + config.EMAIL_USER +
-                " -xp " + passwd.EMAIL_PASSWD +
-                " -u " + subject +
-                " -m " + content))
+     os.system(("sendemail -q -f " + config.EMAIL_FROM + " -t " + config.EMAIL_TO + " -s " + config.EMAIL_SERVER + " -xu " + config.EMAIL_USER + " -xp " + passwd.EMAIL_PASSWD + " -u " + subject + " -m " + content))
