f65417d92281cc80a82daf0bdf0a7f59254fcf92
Johan Vester <jv@jvester.se>
2013-02-16 01:39:15 +0100

    Cleaned up

--- date_actions.py
+++
@@ -1,8 +0,0 @@
-weekdays = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"]
-
-def is_weekday(string):
-    for day in weekdays:
-        if day in string:
-            return True
-        else:
-            return False
