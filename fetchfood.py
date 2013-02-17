d00db6235579f801d5265422d40c09d6c73165d9
Johan Vester <jv@jvester.se>
2013-02-17 22:24:40 +0100

    Change post.post() statements
    Changes in post.py

--- fetchfood.py
+++ fetchfood.py
@@ -72,7 +72,7 @@ exec_times[2] = time.clock()
 entrylist = generate_food_entries(date, content_soup)
 exec_times[2] = time.clock() - exec_times[2]
 exec_times[3] = time.clock()
-post.post(config.POST_URL, config.POST_PAGE, config.POST_HEADERS, passwd.POST_PASSWD, config.ACTION_CLEAR_TABLE) #Clear database table.
+post.post(config.ACTION_CLEAR_TABLE) #Clear database table.
 exec_times[3] = time.clock() - exec_times[3]
 exec_times[4] = time.clock()
 entrycount = post.post_entries(entrylist)
@@ -81,7 +81,7 @@ exec_times[0] = time.clock() - exec_times[0]
 
 for i, time in enumerate(exec_times):
     postdata = {exec_timekeys[i] : time}
-    post.post(config.POST_URL, config.POST_PAGE, config.POST_HEADERS, passwd.POST_PASSWD, config.ACTION_POST_INFO, postdata)
+    post.post(config.ACTION_POST_INFO, postdata)
 
 mail_content = "fetchfood.py successfully ran at:\r\r" + datehelper.current_date() + "\r\rEntries posted: " + str(entrycount)
 exec_times_string_list = [mail_content]
