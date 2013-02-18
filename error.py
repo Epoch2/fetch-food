4eb0fc52e769ef177b4454657894b6685e5c657d
Johan Vester <jv@jvester.se>
2013-02-18 19:22:24 +0100

    Fi x error compiling

--- error.py
+++ error.py
@@ -1,7 +1,8 @@
+import sys
 import mail
 import datehelper
+import post
 import config
-import sys
 
 class ErrorHandler:
 
@@ -10,34 +11,31 @@ class ErrorHandler:
         self.has_error = False
 
     def add_error(self, error, fatal):
-        print fatal
         if fatal:
             self.send_fatal(error)
         self.errorlist.append(error)
         self.has_error = True
 
     def compile_error(self, error):
-        if type(error) == PostException:
-            return error.type_, + " :" + error.exception + " -> " + error.url
+        if type(error) == post.PostException:
+            return error.type_ + " :" + error.exception + " -> " + error.url
+        else:
+            return str(error)
 
     def get_errors_compiled(self, delimiter="\n"):
         errorlist_compiled = []
         for error in self.errorlist:
-            if type(error) == PostException:
-                errorlist_compiled.append(error.type_, + " :" + error.exception + " -> " + error.url + delimiter)
+            errorlist_compiled.append(self.compile_error(error))
         return "".join(errorlist_compiled)
 
     def send_fatal(self, error):
-        print "sendfatal"
-        mail_subject = "FetchFood FAILED!"
-        mail_content = ("A fatal error occurred at:\n\n" +
-                        datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_DATE) + "\n" +
-                        datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_TIME) + "\n\n" +
-                        "FATAL ERROR: " + str(error) + "\n\n" +
-                        "These additional (non-fatal) errors occurred during program execution:" + "\n" +
-                        self.get_errors_compiled("\n"))
         if config.CONFIG_MAIL_ENABLED:
+            mail_subject = "FetchFood FAILED!"
+            mail_content = ("A fatal error occurred at:\n\n" +
+                            datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_DATE) + "\n" +
+                            datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_TIME) + "\n\n" +
+                            "FATAL ERROR: " + self.compile_error(error) + "\n\n" +
+                            "These additional (non-fatal) errors occurred during program execution:" + "\n" +
+                            self.get_errors_compiled("\n"))
             mail.sendmail(mail_subject, mail_content)
-        else:
-            print mail_content
         sys.exit(1)
