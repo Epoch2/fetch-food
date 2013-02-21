f313ef64530d8e4f744b91ee32324c34024e7d1d
Johan Vester <jv@jvester.se>
2013-02-21 02:40:29 +0100

    Added configs for multiweek

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-TARGET_URL = "http://www.amica.se/nackagymnasium"
-TARGET_CONTENT_OUTER_IDENTIFIER = "div#ctl00_RegionPageBody_RegionPage_RegionContent_RegionMainContent_RegionMainContentMiddle_MainContentMenu_ctl00_MenuUpdatePanel"
-TARGET_CONTENT_INNER_IDENTIFIER = "div[class-=ContentArea]"
-TARGET_DATE_IDENTIFIER = "h2#ctl00_RegionPageBody_RegionPage_RegionContent_RegionMainContent_RegionMainContentMiddle_MainContentMenu_ctl00_HeadingMenu"
+TARGET_URL = "http://www.amica.se:80"
+TARGET_SUBURL = "/nackagymnasium"
+TARGET_CONTENT_IDENTIFIER = "div#ctl00_RegionPageBody_RegionPage_RegionContent_RegionMainContent_RegionMainContentMiddle_MainContentMenu_ctl00_MenuUpdatePanel div[class-=ContentArea] p"
+TARGET_CURRENT_DATE_IDENTIFIER = "h2#ctl00_RegionPageBody_RegionPage_RegionContent_RegionMainContent_RegionMainContentMiddle_MainContentMenu_ctl00_HeadingMenu"
+TARGET_SELECTABLE_DATE_IDENTIFIER = "div#ctl00_RegionPageBody_RegionPage_RegionContent_RegionMainContent_RegionMainContentMiddle_MainContentMenu_ctl00_ctl01 select[name=ctl00$RegionPageBody$RegionPage$RegionContent$RegionMainContent$RegionMainContentMiddle$MainContentMenu$ctl00$DropDownListMenuWeek] option"
 
 FOOD_DEFAULT_TYPE = u"Extrarätt"
 FOOD_UNKNOWN_TYPE = "UNKNOWN_TYPE"
@@ -18,9 +19,12 @@ ERROR_POST_INFO_FATAL = False
 POST_TYPE_TYPE = "type"
 POST_TYPE_TIME = "time"
 
+AMICA_TYPE_KEY = "ctl00$RegionPageBody$RegionPage$RegionContent$RegionMainContent$RegionMainContentMiddle$MainContentMenu$ctl00$DropDownListMenu"
+AMICA_WEEK_KEY = "ctl00$RegionPageBody$RegionPage$RegionContent$RegionMainContent$RegionMainContentMiddle$MainContentMenu$ctl00$DropDownListMenuWeek"
+
 POST_URL = "www.portaln.se:80"
-POST_PAGE = "/skola/foodapi.php"
-POST_HEADERS = {"Content-Type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+POST_SUBURL = "/skola/foodapi.php"
+POST_DEFAULT_HEADERS = {"Content-Type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
 EMAIL_SERVER = "send.one.com:2525"
 EMAIL_USER = "server@jvester.se"
