1346fc4f989b84580bb24fbeea885e287e27e1f8
Johan Vester <jv@jvester.se>
2013-02-21 23:52:31 +0100

    Replace compile_error() for str()

--- error.py
+++ error.py
@@ -1,7 +1,6 @@
 import sys
 import mail
 import datehelper
-import post
 import config
 
 class ErrorHandler:
@@ -16,26 +15,20 @@ class ErrorHandler:
         self.errorlist.append(error)
         self.has_error = True
 
-    def compile_error(self, error):
-        if type(error) == post.PostException:
-            return error.type_ + " :" + error.exception + " -> " + error.url
-        else:
-            return str(error)
-
     def get_errors_compiled(self, delimiter="\n"):
         errorlist_compiled = []
         for error in self.errorlist:
-            errorlist_compiled.append(self.compile_error(error))
+            errorlist_compiled.append(str(error))
         return "".join(errorlist_compiled)
 
     def send_fatal(self, error):
-        nl = config.CONFIG_MAIL_NEWLINE
-        if config.CONFIG_MAIL_ENABLED:
+        nl = config.CONFIG["mail_newline"]
+        if config.CONFIG["mail_enabled"]:
             mail_subject = "FetchFood FAILED!"
             mail_content = ("A fatal error occurred at:" + nl + nl +
                             datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_DATE) + nl +
                             datehelper.to_string(datehelper.current_date(), datehelper.PRECISION_TIME) + nl + nl +
-                            "FATAL ERROR: " + self.compile_error(error) + nl + nl +
+                            "FATAL ERROR: " + str(error) + nl + nl +
                             "These additional (non-fatal) errors occurred during program execution:" + nl +
                             self.get_errors_compiled(nl))
             mail.sendmail(mail_subject, mail_content)
