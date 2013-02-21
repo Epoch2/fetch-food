f3a692d3c19e8307ef2972edd989fc1cb063229f
Johan Vester <jv@jvester.se>
2013-02-21 02:40:47 +0100

    Rename functions

--- datehelper.py
+++ datehelper.py
@@ -6,7 +6,7 @@ PRECISION_DATE = "%Y-%m-%d" #2013-05-05
 PRECISION_TIME = "%H:%M:%S" #13:20:22
 WEEKDAYS = [u"Måndag", u"Tisdag", u"Onsdag", u"Torsdag", u"Fredag"]
 
-def weekday_to_weeknumber(string, currentday):
+def weekday_to_weekdaynumber(string, currentday):
     day_of_week = -1
     for day in WEEKDAYS:
         day_of_week += 1
@@ -24,9 +24,11 @@ def weekday_to_date(init_date, day_of_week):
     date = init_date + datetime.timedelta(days=day_of_week)
     return date
 
-def find_date(string):
-    REGEX_DATE = r"^\d{4}\.\d{2}\.\d{1,2}"  #matches dddd.dd.d(d) where d is digit 0-9
-    date_split = re.match(REGEX_DATE, string).group().split(".")
+def to_date(string):
+    REGEX_DATE = r"^\d{4}[\.-_]\d{2}[\.-_]\d{1,2}"  #matches YYYY.MM.DD, YYYY-MM-DD, YYYY_MM_DD
+    REGEX_SEPARATORS = r"[\.-_]+"
+    date_split = re.findall(REGEX_SEPARATORS, re.match(REGEX_DATE, string))
+    #date_split = re.match(REGEX_DATE, string).group().split(".")
     date = datetime.date(int(date_split[0]), int(date_split[1]), int(date_split[2]))
     return date
 
