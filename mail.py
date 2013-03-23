7572a6ec6ab556c2fdc0d7a76529c9c29f34d790
Johan Vester <jv@jvester.se>
2013-03-23 22:08:36 +0100

    Fix mail

--- mail.py
+++ mail.py
@@ -3,5 +3,4 @@ import config
 import passwd
 
 def sendmail(subject, content):
-    return
     os.system("sendemail -q -f " + config.EMAIL_INFO["from"] + " -t " + config.EMAIL_INFO["to"] + " -s " + config.EMAIL_INFO["server"] + " -xu " + config.EMAIL_INFO["user"] + " -xp " + passwd.EMAIL_PASSWD + " -u " + subject + " -m " + content)
